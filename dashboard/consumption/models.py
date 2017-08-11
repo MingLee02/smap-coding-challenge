5c1888c1edf1f164ecd446b1376766a1c5e38c15
MingLee02 <ming.lee@hotmail.com>
2017-08-11 20:00:16 +0100

    Add consumption model, with migration.

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 class UserData(models.Model):
     user_id = models.CharField(max_length=255)
     area = models.CharField(max_length=255)
     tariff = models.CharField(max_length=255)
+
+
+class Consumption(models.Model):
+	datetime = models.DateTimeField(default=timezone.now)
+	Consumption = models.IntegerField(default=0)
