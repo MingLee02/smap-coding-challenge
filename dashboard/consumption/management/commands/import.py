dd313b23b8388cd5f622f5d03ce6935c05faf070
Ming Lee <ming.lee@hotmail.com>
2017-08-12 21:46:30 +0100

    Add import command with test.

--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,5 +1,11 @@
 import os
+
 from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from tablib import Dataset
+
+from consumption.models import Consumption, UserData
 from consumption.utils import get_directory_contents
 
 
@@ -14,20 +20,60 @@ class Command(BaseCommand):
             help='Folder name',
         )
 
+    @staticmethod
+    def open_csv_file(path, filename):
+        """Return contents of csv file"""
+        file = path + '/' + filename
+        with open(file, 'r') as csv_file:
+            contents = csv_file.read()
+        return contents
+
+    def create_user_data(self, path, file):
+        contents = self.open_csv_file(path, file)
+        dataset = Dataset().load(contents)
+        with transaction.atomic():
+            for user_id, area, tariff in dataset:
+                UserData.objects.update_or_create(
+                    user_id=user_id,
+                    area=area,
+                    tariff=tariff
+                )
+
+    def create_consumption_data(self, path, file):
+        contents = self.open_csv_file(path, file)
+        dataset = Dataset().load(contents)
+        with transaction.atomic():
+            for datetime, consumption in dataset:
+                user_id = file.replace('.csv','')
+                user = UserData.objects.get(user_id=user_id)
+                if user:
+                    Consumption.objects.create(
+                        user=user,
+                        datetime=datetime,
+                        consumption=int(float(consumption)),
+                    )
+
     def handle(self, *args, **options):
-        print("Implement me!")
-        folders = []
-        
+        folders = []     
         if options['foldername']:
-            files = get_directory_contents(options['foldername'][0])
+            path, files = get_directory_contents(directory=options['foldername'][0])
         else:
-            files = get_directory_contents()
+            path, files = get_directory_contents()
 
         for file in files:
             is_folder = os.path.isdir(file)
-            
             if is_folder:
                 folders.append(file)
-            else:
-                pass
+            elif '.csv' in file:
+                self.create_user_data(path, file)
+
+        for folder in folders:
+            folder_path, folder_files = get_directory_contents(
+                base_dir=path + '/',
+                directory=folder,
+            )
+
+            for file in folder_files:
+                if '.csv' in file:
+                    self.create_consumption_data(folder_path, file)
                 
--- dashboard/consumption/tests/test_management_commands.py
+++ dashboard/consumption/tests/test_management_commands.py
@@ -1,7 +1,29 @@
 from django.core.management import call_command
+from django.conf import settings
 from django.test import TestCase
+
+from consumption.models import Consumption, UserData
 
 
 class TestImport(TestCase):
-    def test_handle_pass_invalid_parameter(self):
-      pass
+    def test_handle(self):
+        args = 'test_data'
+        call_command('import', args)
+
+        user_one_id = '3011'
+        user_two_id = '3029'
+        user_data = UserData.objects.all()
+
+        self.assertEqual(len(user_data), 2)
+        self.assertEqual(user_data[0].user_id, user_one_id)
+        self.assertEqual(user_data[1].user_id, user_two_id)
+
+        consumption_one_consumption = 3
+        consumption_two_consumption = 2 
+        consumption_data = Consumption.objects.all()
+
+        self.assertEqual(len(consumption_data), 2)
+        self.assertEqual(consumption_data[0].consumption, consumption_one_consumption)
+        self.assertEqual(consumption_data[1].consumption, consumption_two_consumption)
+        self.assertEqual(consumption_data[0].user.user_id, user_one_id)
+        self.assertEqual(consumption_data[1].user.user_id, user_two_id)
