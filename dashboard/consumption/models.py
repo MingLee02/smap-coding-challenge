4b14d2ff445f26110c0696ba4a1b3835589e2c90
Ming Lee <ming.lee@hotmail.com>
2017-08-12 20:58:09 +0100

    Add str method for the consumption model.

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,19 +3,22 @@ from django.utils import timezone
 
 
 class UserData(models.Model):
-    user_id = models.CharField(max_length=255)
-    area = models.CharField(max_length=255)
-    tariff = models.CharField(max_length=255)
+	user_id = models.CharField(max_length=255)
+	area = models.CharField(max_length=255)
+	tariff = models.CharField(max_length=255)
 
-    def __str__(self):
-    	return self.user_id
+	def __str__(self):
+		return self.user_id
 
 
 class Consumption(models.Model):
 	user = models.ForeignKey(
-    	UserData,
-	    on_delete=models.CASCADE,
-	    related_name='users',
+		UserData,
+		on_delete=models.CASCADE,
+		related_name='users',
 	)
 	datetime = models.DateTimeField(default=timezone.now)
 	consumption = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.user.user_id
