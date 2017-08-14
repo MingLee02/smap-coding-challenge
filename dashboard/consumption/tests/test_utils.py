e20332eab414ca0a007e194eaa1ab8954dddc2e0
MingLee02 <ming.lee@hotmail.com>
2017-08-14 18:50:33 +0100

    Remove hardcoded file paths.

--- dashboard/consumption/tests/test_utils.py
+++ dashboard/consumption/tests/test_utils.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.test import TestCase
 
 from ..utils import get_directory_contents
@@ -7,7 +10,8 @@ class TestUtils(TestCase):
     def test_get_directory(self):
         """Assert default folder contents can be gotten"""
         response = get_directory_contents()
-        expected_path = '/Users/mlee/Sites/smap-coding-challenge/data'
+        root_path = os.path.abspath(os.path.join(settings.BASE_DIR, os.pardir))
+        expected_path = os.path.join(root_path, 'data')
         expected_files = ['consumption', 'user_data.csv']
 
         expected_response = expected_path, expected_files
@@ -19,16 +23,19 @@ class TestUtils(TestCase):
         response = get_directory_contents(
             directory='test_data'
         )
-
-        expected_url = '/Users/mlee/Sites/smap-coding-challenge/test_data'
+        root_path = os.path.abspath(os.path.join(settings.BASE_DIR, os.pardir))
+        expected_url = os.path.join(root_path, 'test_data')
         expected = (expected_url, ['consumption', 'user_data.csv'])
         
         self.assertEqual(response, expected)
 
     def test_get_directory_with_incorrect_parameter(self):
         response = get_directory_contents(directory='fakkkkkeee')
+
+        root_path = os.path.abspath(os.path.join(settings.BASE_DIR, os.pardir))
+        expected_path = os.path.join(root_path, 'fakkkkkeee')
         
         self.assertEqual(
             response,
-            ('/Users/mlee/Sites/smap-coding-challenge/fakkkkkeee', [])
+            (expected_path, [])
         )
