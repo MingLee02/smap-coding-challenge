380ed267e63842158ad0740ba26921f40e2d9ea6
MingLee02 <ming.lee@hotmail.com>
2017-08-16 19:04:16 +0100

    Fix indentation.

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,22 +3,24 @@ from django.utils import timezone
 
 
 class UserData(models.Model):
-	user_id = models.CharField(max_length=255)
-	area = models.CharField(max_length=255)
-	tariff = models.CharField(max_length=255)
+    user_id = models.CharField(max_length=255)
+    area = models.CharField(max_length=255)
+    tariff = models.CharField(max_length=255)
 
-	def __str__(self):
-		return self.user_id
+    def __str__(self):
+        return self.user_id
 
+    class Meta:
+        ordering = ['user_id']
 
 class Consumption(models.Model):
-	user = models.ForeignKey(
-		UserData,
-		on_delete=models.CASCADE,
-		related_name='users',
-	)
-	datetime = models.DateTimeField(default=timezone.now)
-	consumption = models.IntegerField(default=0)
+    user = models.ForeignKey(
+        UserData,
+        on_delete=models.CASCADE,
+        related_name='users',
+    )
+    datetime = models.DateTimeField(default=timezone.now)
+    consumption = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.user.user_id
+    def __str__(self):
+        return self.user.user_id
